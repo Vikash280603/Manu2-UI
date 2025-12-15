52f6faa7629ee850e7c7091e73c820e140fe1abb
Balaji G <2463440@cognizant.com>
2025-12-15 14:50:26 +0530

    Added js objected

---
+++ src/modules/product-bom/entities/bom.js
@@ -0,0 +1,23 @@
+
+[
+  {
+    "Id": 301,
+    "Name": "Steel Rod",
+    "quantity": 10
+  },
+  {
+    "Id": 302,
+    "Name": "Aluminum Sheet",
+    "quantity": 5
+  },
+  {
+    "Id": 303,
+    "Name": "Plastic Cover",
+    "quantity": 20
+  },
+  {
+    "Id": 304,
+    "Name": "Copper Wire",
+    "quantity": 50
+  }
+]
---
+++ src/modules/product-bom/entities/product.js
@@ -0,0 +1,17 @@
+
+[
+  {
+    "productId": 101,
+    "bomList": [
+      { "Id": 301, "Name": "Steel Rod", "quantity": 10 },
+      { "Id": 302, "Name": "Aluminum Sheet", "quantity": 5 }
+    ]
+  },
+  {
+    "productId": 102,
+    "bomList": [
+      { "Id": 303, "Name": "Plastic Cover", "quantity": 20 },
+      { "Id": 304, "Name": "Copper Wire", "quantity": 50 }
+    ]
+  }
+]
